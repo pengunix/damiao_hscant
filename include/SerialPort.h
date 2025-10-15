7ead86fbb1f14a73f60a119f7dc4af8f3495ec2b
pengunix <hoshimaemi@outlook.com>
2025-10-15 11:05:48 +0000

    主要控制指令、电机状态反馈正常

---
+++ include/SerialPort.h
@@ -0,0 +1,148 @@
+#ifndef SERIAL_PORT_H
+#define SERIAL_PORT_H
+#include <array>
+#include <chrono>
+#include <memory>
+#include <queue>
+#include <string>
+#include <cstring>
+#include <unistd.h>
+
+namespace Serial {
+
+extern "C" {
+#include <fcntl.h>
+#include <linux/serial.h>
+#include <sys/ioctl.h>
+#include <sys/select.h>
+#include <termios.h>
+}
+
+class SerialPort {
+public:
+  using SharedPtr = std::shared_ptr<SerialPort>;
+
+  SerialPort(std::string port, speed_t baudrate, int timeout_ms = 2) {
+    set_timeout(timeout_ms);
+    Init(port, baudrate);
+  }
+
+  ~SerialPort() { close(); }
+
+  ssize_t send(const uint8_t *data, size_t len) {
+    // tcflush(fd_, TCIFLUSH);
+    ssize_t ret = ::write(fd_, data, len);
+    // tcdrain(fd_);
+    return ret;
+  }
+
+  ssize_t recv(uint8_t *data, size_t len) {
+    FD_ZERO(&rSet_);
+    FD_SET(fd_, &rSet_);
+    ssize_t recv_len = 0;
+
+    switch (select(fd_ + 1, &rSet_, NULL, NULL, &timeout_)) {
+    // TODO(me): 改logger
+    case -1: // error
+      // std::cout << "communication error" << std::endl;
+      break;
+    case 0: // timeout
+      // std::cout << "timeout" << std::endl;
+      break;
+    default:
+      recv_len = ::read(fd_, data, len);
+      if (recv_len < 0) {
+        // std::cout << "read error" << std::endl;
+      }
+      break;
+    }
+    return recv_len;
+  }
+
+  void recv(uint8_t *data, uint8_t head, ssize_t len) {
+    // 存入队列
+    ssize_t recv_len = this->recv(recv_buf.data(), len);
+    for (int i = 0; i < recv_len; i++) {
+      recv_queue.push(recv_buf[i]);
+    }
+
+    // 查找帧头
+    while (recv_queue.size() >= len) {
+      if (recv_queue.front() != head) {
+        recv_queue.pop();
+        continue;
+      }
+      break;
+    }
+
+    if (recv_queue.size() < len)
+      return;
+
+    // 读取数据
+    for (int i = 0; i < len; i++) {
+      data[i] = recv_queue.front();
+      recv_queue.pop();
+    }
+  }
+
+  void set_timeout(int timeout_ms) {
+    timeout_.tv_sec = timeout_ms / 1000;
+    timeout_.tv_usec = (timeout_ms % 1000) * 1000;
+  }
+
+  bool is_Open() const { return is_open; }
+
+  void close() { ::close(fd_); }
+
+private:
+  void Init(std::string port, speed_t baudrate) {
+    int ret;
+    // Open serial port
+    fd_ = open(port.c_str(), O_RDWR | O_NOCTTY);
+    if (fd_ < 0) {
+      printf("Open serial port %s failed\n", port.c_str());
+      is_open = false;
+      exit(-1);
+    }
+    is_open = true;
+    // Set attributes
+    struct termios option;
+    memset(&option, 0, sizeof(option));
+    ret = tcgetattr(fd_, &option);
+
+    option.c_oflag = 0;
+    option.c_lflag = 0;
+    option.c_iflag = 0;
+
+    cfsetispeed(&option, baudrate);
+    cfsetospeed(&option, baudrate);
+
+    option.c_cflag &= ~CSIZE;
+    option.c_cflag |= CS8;     // 8
+    option.c_cflag &= ~PARENB; // no parity
+    option.c_iflag &= ~INPCK;  // no parity
+    option.c_cflag &= ~CSTOPB; // 1 stop bit
+    // 关闭硬件流控
+    option.c_cflag &= ~CRTSCTS;
+    option.c_iflag &= ~(IXON | IXOFF | IXANY);
+
+    option.c_cc[VTIME] = 0;
+    option.c_cc[VMIN] = 0;
+    option.c_lflag |= CBAUDEX;
+
+    ret = tcflush(fd_, TCIFLUSH);
+    ret = tcsetattr(fd_, TCSANOW, &option);
+  }
+
+  int fd_;
+  fd_set rSet_;
+  timeval timeout_;
+  bool is_open;
+
+  std::queue<uint8_t> recv_queue;
+  std::array<uint8_t, 1024> recv_buf;
+};
+
+} // namespace Serial
+
+#endif // SERIAL_PORT_H
---
+++ include/damiao.h
@@ -0,0 +1,390 @@
+#ifndef DAMIAO_H
+#define DAMIAO_H
+
+#include "SerialPort.h"
+#include <array>
+#include <cmath>
+#include <cstdint>
+#include <functional>
+#include <thread>
+#include <unordered_map>
+#include <utility>
+#include <variant>
+#include <vector>
+#include <atomic>
+
+#define POS_MODE 0x100
+#define SPEED_MODE 0x200
+#define POSI_MODE 0x300
+#define max_retries 20
+#define retry_interval 50000
+
+namespace damiao {
+
+#pragma pack(1)
+#define Motor_id uint32_t
+
+/*!
+ * @brief Motor Type 电机类型
+ */
+enum DM_Motor_Type {
+  DM4310,
+  DM4310_48V,
+  DM4340,
+  DM4340_48V,
+  DM6006,
+  DM8006,
+  DM8009,
+  DM10010L,
+  DM10010,
+  DMH3510,
+  DMH6215,
+  DMG6220,
+  Num_Of_Motor
+};
+
+/*
+ * @brief 电机控制模式
+ */
+enum Control_Mode {
+  MIT_MODE = 1,
+  POS_VEL_MODE = 2,
+  VEL_MODE = 3,
+  POS_FORCE_MODE = 4,
+};
+
+/*
+ * @brief 寄存器列表 具体参考达妙手册
+ */
+enum DM_REG {
+  UV_Value = 0,
+  KT_Value = 1,
+  OT_Value = 2,
+  OC_Value = 3,
+  ACC = 4,
+  DEC = 5,
+  MAX_SPD = 6,
+  MST_ID = 7,
+  ESC_ID = 8,
+  TIMEOUT = 9,
+  CTRL_MODE = 10,
+  Damp = 11,
+  Inertia = 12,
+  hw_ver = 13,
+  sw_ver = 14,
+  SN = 15,
+  NPP = 16,
+  Rs = 17,
+  LS = 18,
+  Flux = 19,
+  Gr = 20,
+  PMAX = 21,
+  VMAX = 22,
+  TMAX = 23,
+  I_BW = 24,
+  KP_ASR = 25,
+  KI_ASR = 26,
+  KP_APR = 27,
+  KI_APR = 28,
+  OV_Value = 29,
+  GREF = 30,
+  Deta = 31,
+  V_BW = 32,
+  IQ_c1 = 33,
+  VL_c1 = 34,
+  can_br = 35,
+  sub_ver = 36,
+  u_off = 50,
+  v_off = 51,
+  k1 = 52,
+  k2 = 53,
+  m_off = 54,
+  dir = 55,
+  p_m = 80,
+  xout = 81,
+};
+
+#pragma pack(push, 1)
+typedef struct {
+  uint8_t FrameHeader;
+  // uint8_t CMD;// 命令 0x00: 心跳
+  //     0x01: receive fail 0x11: receive success
+  //     0x02: send fail 0x12: send success
+  //     0x03: set baudrate fail 0x13: set baudrate success
+  //     0xEE: communication error 此时格式段为错误码
+  //     8: 超压 9: 欠压 A: 过流 B: MOS过温 C: 电机线圈过温 D: 通讯丢失 E: 过载
+  // uint8_t canDataLen: 6; // 数据长度
+  // uint8_t canIde: 1; // 0: 标准帧 1: 扩展帧
+  // uint8_t canRtr: 1; // 0: 数据帧 1: 远程帧
+  uint8_t canId[3]; // 电机反馈的ID
+  uint8_t len;      // len
+  uint8_t canData[16];
+  uint8_t frameEnd; // 帧尾
+} CAN_Receive_Frame;
+#pragma pack(pop)
+
+typedef struct can_send_frame {
+  uint8_t FrameHeader = 0x74; // 帧头
+  uint8_t canId[3] = {0};     // CAN ID 使用电机ID作为CAN ID
+  uint8_t len = 0x38;         // len
+  uint8_t data[16] = {0};
+  uint8_t frameEnd = 0x0D;
+
+  void modify(const Motor_id id, const uint8_t *send_data) {
+    // canId = id;
+    // std::copy(send_data, send_data+8, data);
+
+    canId[0] = '0' + (id / 100);       // 百位
+    canId[1] = '0' + ((id / 10) % 10); // 十位
+    canId[2] = '0' + (id % 10);        // 个位
+    const char hexChars[] = "0123456789ABCDEF";
+    for (int i = 0; i < 8; i++) {
+      uint8_t byte = send_data[i];
+      data[2 * i] = hexChars[byte >> 4];       // 高4位
+      data[2 * i + 1] = hexChars[byte & 0x0F]; // 低4位
+    }
+  }
+
+} can_send_frame;
+
+#pragma pack()
+
+typedef struct {
+  float Q_MAX;
+  float DQ_MAX;
+  float TAU_MAX;
+} Limit_param;
+
+//电机PMAX DQMAX TAUMAX参数
+extern Limit_param limit_param[Num_Of_Motor];
+
+struct DmActData {
+  std::string name;
+  DM_Motor_Type motorType;
+  int can_id;
+  int mst_id;
+  double pos, vel, effort;
+  double cmd_pos, cmd_vel, cmd_effort;
+  double kp, kd;
+};
+
+class Motor {
+private:
+  /* data */
+  Motor_id Master_id;
+  Motor_id Slave_id;
+  float state_q = 0;
+  float state_dq = 0;
+  float state_tau = 0;
+  Limit_param limit_param{};
+  DM_Motor_Type Motor_Type;
+
+  union ValueUnion {
+    float floatValue;
+    uint32_t uint32Value;
+  };
+
+  struct ValueType {
+    ValueUnion value;
+    bool isFloat;
+  };
+
+  std::unordered_map<uint32_t, ValueType> param_map;
+
+public:
+  /**
+   * @brief Construct a new Motor object
+   *
+   * @param Motor_Type 电机类型
+   * @param Slave_id canId 从机ID即电机ID
+   * @param Master_id 主机ID建议主机ID不要都设为0x00
+   *
+   */
+  Motor(DM_Motor_Type Motor_Type, Motor_id Slave_id, Motor_id Master_id);
+
+  void receive_data(float q, float dq, float tau);
+
+  DM_Motor_Type GetMotorType() const { return this->Motor_Type; }
+
+  /*
+   * @brief get master id 获取主机ID
+   * @return MasterID
+   */
+  Motor_id GetMasterId() const { return this->Master_id; }
+
+  /*
+   * @brief get motor slave id(can id)  获取电机CAN ID
+   * @return SlaveID
+   */
+  Motor_id GetSlaveId() const { return this->Slave_id; }
+
+  float Get_Position() const { return this->state_q; }
+
+  float Get_Velocity() const { return this->state_dq; }
+
+  float Get_tau() const { return this->state_tau; }
+
+  /*
+   * @brief get limit param 获取电机限制参数
+   * @return limit_param 电机限制参数
+   */
+  Limit_param get_limit_param() { return limit_param; }
+
+  void set_param(int key, float value);
+
+  void set_param(int key, uint32_t value);
+
+  float get_param_as_float(int key) const;
+
+  uint32_t get_param_as_uint32(int key) const;
+
+  bool is_have_param(int key) const;
+};
+
+/**
+ * @brief motor control class 电机控制类
+ *
+ * 使用USB转CAN进行通信，linux做虚拟串口
+ */
+class Motor_Control {
+public:
+  /*
+   * @brief 定义电机控制对象
+   * @param serial 串口对象
+   * 默认串口为/dev/ttyACM0
+   */
+  Motor_Control(const std::string &serial_port, Serial::speed_t seial_baud);
+    //  const std::unordered_map<Motor_id, DmActData>& dmact_data);
+
+  ~Motor_Control();
+  //读取串口电机数据线程
+  void get_motor_data_thread();
+  /*
+   * @brief enable the motor 使能电机
+   * @param motor 电机对象
+   */
+  void enable();
+
+  void hscant_init(const std::string &_port);
+  // void write();
+  // void read();
+  /*
+   * @brief refresh motor status 刷新电机状态
+   * @param motor object 电机对象
+   */
+  void refresh_motor_status(const Motor &motor);
+
+  void disable();
+  void set_zero_position();
+
+  void control_mit(Motor &DM_Motor, float kp, float kd, float q, float dq,
+                   float tau);
+  void control_pos_vel(Motor &DM_Motor, float pos, float vel);
+  void control_vel(Motor &DM_Motor, float vel);
+
+  /*
+   * @description 函数库内部调用,用于解算电机can线返回的电机参数
+   */
+  void receive();
+
+  void receive_param();
+
+  /**
+   * @brief add motor to class 添加电机
+   * @param DM_Motor : motor object 电机对象
+   */
+  void addMotor(Motor *DM_Motor);
+
+  /*
+   * @description: read motor register param
+   * 读取电机内部寄存器参数，具体寄存器列表请参考达妙的手册
+   * @param DM_Motor: motor object 电机对象
+   * @param RID: register id 寄存器ID  example: damiao::UV_Value
+   * @return: motor param 电机参数 如果没查询到返回的参数为0
+   */
+  float read_motor_param(Motor &DM_Motor, uint8_t RID);
+
+  /*
+   * @description: switch control mode 切换电机控制模式
+   * @param DM_Motor: motor object 电机对象
+   * @param mode: control mode 控制模式 like:damiao::MIT_MODE,
+   * damiao::POS_VEL_MODE, damiao::VEL_MODE, damiao::POS_FORCE_MODE
+   */
+  bool switchControlMode(Motor &DM_Motor, Control_Mode mode);
+
+  /*
+   * @description: change motor param 修改电机内部寄存器参数
+   * 具体寄存器列表请参考达妙手册
+   * @param DM_Motor: motor object 电机对象
+   * @param RID: register id 寄存器ID
+   * @param data: param data
+   * 参数数据,大部分数据是float类型，其中如果是uint32类型的数据也可以直接输入整型的就行，函数内部有处理
+   * @return: bool true or false  是否修改成功
+   */
+  bool change_motor_param(Motor &DM_Motor, uint8_t RID, float data);
+
+  /*
+   * @description: save all param to motor flash 保存电机的所有参数到flash里面
+   * @param DM_Motor: motor object 电机对象
+   * 电机默认参数不会写到flash里面，需要进行写操作
+   */
+  void save_motor_param(Motor &DM_Motor);
+
+  /*
+   * @description: change motor limit param
+   * 修改电机限制参数，这个修改的不是电机内部的寄存器参数，而是电机的限制参数
+   * @param DM_Motor: motor object 电机对象
+   * @param P_MAX: position max 位置最大值
+   * @param Q_MAX: velocity max 速度最大值
+   * @param T_MAX: torque max 扭矩最大值
+   */
+  static void changeMotorLimit(Motor &DM_Motor, float P_MAX, float Q_MAX,
+                               float T_MAX);
+
+private:
+  void control_cmd(Motor_id id, uint8_t cmd);
+
+  void write_motor_param(Motor &DM_Motor, uint8_t RID, const uint8_t data[4]);
+  static bool is_in_ranges(int number) {
+    return (7 <= number && number <= 10) || (13 <= number && number <= 16) ||
+           (35 <= number && number <= 36);
+  }
+
+  static uint32_t float_to_uint32(float value) {
+    return static_cast<uint32_t>(value);
+  }
+
+  static float uint32_to_float(uint32_t value) {
+    return static_cast<float>(value);
+  }
+
+  static float uint8_to_float(const uint8_t data[4]) {
+    uint32_t combined = (static_cast<uint32_t>(data[3]) << 24) |
+                        (static_cast<uint32_t>(data[2]) << 16) |
+                        (static_cast<uint32_t>(data[1]) << 8) |
+                        static_cast<uint32_t>(data[0]);
+    float result;
+    memcpy(&result, &combined, sizeof(result));
+    return result;
+  }
+  std::thread rec_thread;
+  // std::unique_ptr<std::thread> rec_thread;
+
+  std::unordered_map<Motor_id, Motor *> motors;
+  std::unique_ptr<Serial::SerialPort> serial_;
+
+  // ! 似乎是一个由用户维护的数据，用于集中管理单个串口的多个电机
+  // ! 配套的api有MotorContorl构造函数、write、read函数
+  // TODO(me): 暂时不用这样暴露整个数据，后续修改
+  std::unordered_map<Motor_id, DmActData> dmact_data;
+
+  // bool stop_thread_ = false;
+  std::atomic<bool> stop_thread_ = true;
+
+  can_send_frame send_data;         // send data frame
+  CAN_Receive_Frame receive_data{}; // receive data frame
+};
+
+}; // namespace damiao
+
+#endif
