f2355e8c587d969314099cacdd7fd1ebabe410d9
pengunix <hoshimaemi@outlook.com>
2026-03-12 21:32:01 +0800

    ✨ feat: Restart Can when start

--- scripts/setup_can.sh
+++ scripts/setup_can.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+sudo ip link set up can0
+sudo ip link set up can1
+sudo ip link set up can2
+sudo ip link set up can3
 sudo ip link set can0 type can bitrate 1000000 
 sudo ip link set can1 type can bitrate 1000000 
 sudo ip link set can2 type can bitrate 1000000 
